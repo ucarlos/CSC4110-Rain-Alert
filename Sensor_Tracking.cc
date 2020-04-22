67283e649b980536d9ff0ab4e83c20c7bf4ee203
Ulysses Carlos <ucarlos1@student.gsu.edu>
2020-04-22 17:09:54 -0400

    Added Sensor_Tracking.cc

---
+++ Sensor_Tracking.cc
@@ -0,0 +1,21 @@
+/*
+ * -----------------------------------------------------------------------------
+ * Created by Ulysses Carlos on 04/22/2020 at 03:24 PM
+ * 
+ * Sensor_Tracking.cc
+ * 
+ * I've separated all the functions related to the sensor tracking into
+ * a single c++ file.
+ * -----------------------------------------------------------------------------
+ */
+
+#include "./Project.h"
+
+/*
+
+  So the idea is this:
+  there's a funt
+
+
+
+ */
